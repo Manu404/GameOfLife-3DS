0cf04c4c73c5a58313ff3ef0ade6d56745d02499
Manu404 <istace.emmanuel@hotmail.com>
2018-05-22 23:50:53 +0200

    Fix crash on the 3ds hardware

--- source/main.cpp
+++ source/main.cpp
@@ -7,12 +7,13 @@
 #include <unistd.h>
 #include <logo_bgr.h>
 
-#define WIDTH 5
-#define HEIGTH 5
+#define WIDTH 400
+#define HEIGTH 240
 #define PIXELBUFFER_SIZE 96000 // 96000
 
 u8 world_framebuffer[PIXELBUFFER_SIZE * 3]; // RGB RGB RGB RGB ... so times 3
 u32 world_framebuffer_size = PIXELBUFFER_SIZE * 3; 
+
 
 class Vector2 // 2D Vector
 {
@@ -30,6 +31,7 @@ public:
 class Cell // Conway cell
 {
     Cell* neighbours[8];
+    std::list<Cell*> neighbours2;
     int newState; 
     int currentNeighbors = 0;
 public:
@@ -40,26 +42,61 @@ public:
     {
         this->IsAlive = isAlive;
         this->Position = position;
+        newState = IsAlive;
         memset(neighbours, 0, (8 * sizeof(Cell*)));
+    }
+
+    int GetState()
+    {
+        return IsAlive;
     }
 
     void AddNeighbourgs(Cell* cell)
     {
         neighbours[currentNeighbors] = cell;
         currentNeighbors += 1;
+        neighbours2.push_back(cell);
     }
 
-    void ComputeState()
+    void ComputeState(Cell* cells [WIDTH][HEIGTH])
     {
         //this->WasAlive = IsAlive;
         newState = IsAlive;
 
         // Compute alive cells
         int aliveCellCount = 0;
-        if (currentNeighbors == 0) return;
+        /*if (currentNeighbors == 0) return;
         for (int x = 0; x < currentNeighbors; x++)
             if (neighbours[x]->IsAlive)
+                aliveCellCount += 1;*/
+        std::list<Cell*>::const_iterator iterator;
+
+        for (iterator = neighbours2.begin(); iterator != neighbours2.end(); ++iterator) {
+            if ((*iterator)->GetState())
                 aliveCellCount += 1;
+        }
+
+        /*Vector2* top_left = AdjustCoordonates(new Vector2(Position->X + 1, Position->Y - 1));
+        Vector2* top_center = AdjustCoordonates(new Vector2(Position->X + 1, Position->Y));
+        Vector2* top_right = AdjustCoordonates(new Vector2(Position->X + 1, Position->Y + 1));
+
+        Vector2* middle_left = AdjustCoordonates(new Vector2(Position->X, Position->Y - 1));
+        Vector2* middle_right = AdjustCoordonates(new Vector2(Position->X, Position->Y + 1));
+
+        Vector2* bottom_left = AdjustCoordonates(new Vector2(Position->X - 1, Position->Y - 1));
+        Vector2* bottom_center = AdjustCoordonates(new Vector2(Position->X - 1, Position->Y));
+        Vector2* bottom_right = AdjustCoordonates(new Vector2(Position->X - 1, Position->Y + 1));
+
+        if(cells[top_left->X][top_left->Y]->IsAlive)aliveCellCount+=1;
+        if(cells[top_center->X][top_center->Y]->IsAlive)aliveCellCount += 1;
+        if(cells[top_right->X][top_right->Y]->IsAlive)aliveCellCount += 1;
+        
+        if(cells[middle_left->X][middle_left->Y]->IsAlive)aliveCellCount += 1;
+        if(cells[middle_right->X][middle_right->Y]->IsAlive)aliveCellCount += 1;
+        
+        if(cells[bottom_left->X][bottom_left->Y]->IsAlive)aliveCellCount += 1;
+        if(cells[bottom_center->X][bottom_center->Y]->IsAlive)aliveCellCount += 1;
+        if(cells[bottom_right->X][bottom_right->Y]->IsAlive)aliveCellCount += 1;*/
         
         // Apply Conways lay
         if (aliveCellCount < 2 && this->IsAlive) newState = 0;
@@ -68,22 +105,33 @@ public:
         else if (aliveCellCount == 3 && !this->IsAlive) newState = 1;
     }
 
+    Vector2* AdjustCoordonates(Vector2* vector)
+    {
+        if (vector->X >= WIDTH) vector->X = 0;
+        else if (vector->X < 0) vector->X = WIDTH - 1;
+        if (vector->Y >= HEIGTH) vector->Y = 0;
+        else if (vector->Y < 0) vector->Y = HEIGTH - 1;
+        return vector;
+    }
+
     void ApplyNewState()
     {
         this->IsAlive = this->newState;
     }
 };
 
+
 class World
 {
     Vector2* size;
-    Thread computeThread[WIDTH];
-    Cell* cells[WIDTH][HEIGTH];
 public:
+
+    Cell * cells[WIDTH][HEIGTH];
+
     World(Vector2* size)
     {
         this->size = size;
-        memset(cells, 0, (size->X * size->Y * sizeof(Cell*)));
+        //memset(cells, 0, (size->X * size->Y * sizeof(Cell*)));
     }
 
     void GenerateCells()
@@ -128,9 +176,9 @@ public:
     Vector2* AdjustCoordonates(Vector2* vector)
     {
         if (vector->X >= this->size->X) vector->X = 0;
-        else if (vector->X < 0) vector->X = this->size->Y;
+        else if (vector->X < 0) vector->X = this->size->Y - 1;
         if (vector->Y >= this->size->Y) vector->Y = 0;
-        else if (vector->Y < 0) vector->Y = this->size->Y;
+        else if (vector->Y < 0) vector->Y = this->size->Y - 1;
         return vector;
     }
 
@@ -139,7 +187,7 @@ public:
         for (int x = 0; x < size->X; x++)
             for (int y = 0; y < size->Y; y++)
             {
-                cells[x][y]->ComputeState();
+                cells[x][y]->ComputeState(cells);
             }
     }
 
@@ -153,7 +201,6 @@ public:
 
                 // RGB so times three
                 int memOffset = ((x * this->size->Y) + (y)) * 3;
-                //if (memOffset > PIXELBUFFER_SIZE * 3) continue;
                 world_framebuffer[memOffset] = color;// ? rand() % 255 : 0;
                 world_framebuffer[memOffset + 1] = color;// ? rand() % 255 : 0;
                 world_framebuffer[memOffset + 2] = color;// ? rand() % 255 : 0;*/
@@ -184,18 +231,14 @@ int main(int argc, char **argv)
     World* w = new World(new Vector2(WIDTH, HEIGTH));
     w->GenerateCells();
     w->PopulateNeighbourgs();
+    w->Compute();
 
     int frameCount = 0;
 	// Main loop
 	while (aptMainLoop())
 	{
-        frameCount += 1;
-        //if(frameCount % 60 == 0)
-        {
-            w->Compute();            
-        }
-
         w->Print();
+        w->Compute();
 
         //Copy our image in the bottom screen's frame buffer
         memcpy(fb, world_framebuffer, world_framebuffer_size);
